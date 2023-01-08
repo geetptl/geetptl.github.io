9f7df67882ce69d3e60c707ec8b2d39907ca6f72
Geet Rajendra Patel <geetpofficial@gmail.com>
2023-01-08 11:18:38 -0500

    rel="noopener noreferrer

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,9 @@ export default function Home() {
           <nav className='p-10 mb-12 flex justify-between'>
             <h1 className='text-4xl'>Geet Patel</h1>
             <ul className='flex items-center'>
-              <li className='px-2'><a href='https://github.com/geetptl' title='Github' target="_blank"><FaGithubSquare size={40} /></a></li>
-              <li className='px-2'><a href='https://www.linkedin.com/in/geet-p/' title='Linkedin' target="_blank"><FaLinkedin size={40} /></a></li>
-              <li><a className=' bg-gradient-to-br from-slate-500 to-slate-700 px-4 py-2 rounded-md ml-2' href='./resume.pdf' title='Single-page Resume' target='_blank'>Resume</a></li>
+            <li className='px-2'><a href='https://github.com/geetptl' title='Github' target="_blank" rel="noopener noreferrer"><FaGithubSquare size={40} /></a></li>
+              <li className='px-2'><a href='https://www.linkedin.com/in/geet-p/' title='Linkedin' target="_blank" rel="noopener noreferrer"><FaLinkedin size={40} /></a></li>
+              <li><a className=' bg-gradient-to-br from-slate-500 to-slate-700 px-4 py-2 rounded-md ml-2' href='./resume.pdf' title='Single-page Resume' target='_blank' rel="noopener noreferrer">Resume</a></li>
             </ul>
           </nav>
         </section>
