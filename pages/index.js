581e6c1689ad906d4074def6657e50d58e3f7528
Geet Rajendra Patel <geetpofficial@gmail.com>
2023-01-08 11:15:41 -0500

    Font change, adding links

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import {FaGithubSquare} from 'react-icons/fa'
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,12 +14,13 @@ export default function Home() {
       </Head>
 
       <main className='bg-slate-600 px-10'>
-        <section className='bg-slate-900 min-h-screen text-rose-100'>
+        <section className='bg-slate-800 min-h-screen text-rose-100'>
           <nav className='p-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Geet Patel</h1>
+            <h1 className='text-4xl'>Geet Patel</h1>
             <ul className='flex items-center'>
-              <li><FaGithubSquare size={40}/></li>
-              <li><a className=' bg-gradient-to-br from-slate-500 to-slate-700 text-rose-300 px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
+              <li className='px-2'><a href='https://github.com/geetptl' title='Github' target="_blank"><FaGithubSquare size={40} /></a></li>
+              <li className='px-2'><a href='https://www.linkedin.com/in/geet-p/' title='Linkedin' target="_blank"><FaLinkedin size={40} /></a></li>
+              <li><a className=' bg-gradient-to-br from-slate-500 to-slate-700 px-4 py-2 rounded-md ml-2' href='./resume.pdf' title='Single-page Resume' target='_blank'>Resume</a></li>
             </ul>
           </nav>
         </section>
