13271e3e669efd0548f7395f54fd6f8122a18792
Geet Rajendra Patel <geetpofficial@gmail.com>
2023-04-25 19:59:48 -0400

    New: Fixing content sizes, adding know more button

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 import Head from 'next/head'
+import { AiOutlineDown } from 'react-icons/ai'
 import { FiCopy, FiGithub } from 'react-icons/fi'
 import { FaLinkedinIn } from 'react-icons/fa'
 import { BrowserView, MobileView } from 'react-device-detect';
 import React, { useState } from 'react'
 
 export default function Home() {
-  const [showAlert, setShowAlert] = useState(false);
+  const [showEmailAlert, setShowEmailAlert] = useState(false);
 
-  const handleButtonClick = () => {
-    setShowAlert(true);
+  const handleEmailClick = () => {
+    setShowEmailAlert(true);
     setTimeout(() => {
-      setShowAlert(false);
+      setShowEmailAlert(false);
+    }, 2000);
+  };
+
+  const [showKnowMoreAlert, setKnowMoreAlert] = useState(false);
+
+  const handleKnowMoreClick = () => {
+    setKnowMoreAlert(true);
+    setTimeout(() => {
+      setKnowMoreAlert(false);
     }, 2000);
   };
 
@@ -43,28 +53,42 @@ export default function Home() {
               </ul>
             </nav>
 
-            <div className='text-left px-40 py-10'>
-              <text className='text-4xl text-slate-200'>Hello there!</text>
-              <div className='text-3xl text-slate-300 py-10'>
-                <h2>I am <text className=''>Geet Patel</text>. I am a grad student in Computer Science at Rutgers.</h2>
-                <h2 className='py-5'>I am passionate about developing scalable backend solutions and robust cloud infrastructure.</h2>
-                <h2>But that&apos;s barely scratching the surface.</h2>
-                <h2 className='py-5'>I have also provided vital oral contributions in UX design and data visualization products.</h2>
-              </div>
-              <div className='text-slate-200 py-10'>
-                <h2><text className='text-lg'>Get in touch</text>
-                  <button className='text-sm align-top px-2 py-1 rounded-md ml-2 transition ease-in-out delay-100 bg-gradient-to-br text-l from-white to-gray-200 text-blue-700 hover:scale-125 hover:translate-x-5 duration-300' onClick={handleButtonClick}>geetpofficial@gmail.com</button></h2>{
-                  showAlert && (
-                    <div className='fixed bottom-10 right-20 bg-teal-800 text-teal-200 px-3 py-2 rounded animate-fade-in-out'>
-                      Email copied!
-                    </div>
-                  )
-                }
-              </div>
+            <div className='text-left py-10 px-40 text-4xl text-slate-200'>
+              <text className=''>Hello there!</text>
             </div>
-          </BrowserView>
-        </section>
-      </main>
-    </div>
+
+            <div className='text-left px-40 text-3xl text-slate-300 mt-5 space-y-5 w-4/5 max-w-[3/5]'>
+              <p>I am Geet Patel. I am a grad student in Computer Science at Rutgers.</p>
+              <p>I am passionate about developing scalable backend solutions and robust cloud infrastructure.</p>
+              <p>But that&apos;s barely scratching the surface.</p>
+            </div>
+
+            <div className='text-left px-40 fixed bottom-20 text-slate-200 space-y-10'>
+              <p>
+                <text className='text-lg'>Get in touch</text>
+                <button className='text-sm align-top px-2 py-1 rounded-md ml-2 transition ease-in-out delay-100 bg-gradient-to-br text-l from-white to-gray-200 text-blue-700 hover:scale-110 hover:translate-x-2 duration-300' onClick={handleEmailClick}>geetpofficial@gmail.com</button>
+              </p>
+              {
+                showEmailAlert && (
+                  <div className='fixed bottom-10 right-20 bg-teal-800 text-teal-200 px-3 py-2 rounded animate-fade-in-out'>
+                    Email copied!
+                  </div>
+                )
+              }
+              <button className='text-slate-200 text-xl' onClick={handleKnowMoreClick}>
+                Or know me better<AiOutlineDown size={30} />
+              </button>
+              {
+                showKnowMoreAlert && (
+                  <div className='fixed bottom-10 right-20 bg-red-500 text-red-200 px-3 py-2 rounded animate-fade-in-out'>
+                    That's under construction!
+                  </div>
+                )
+              }
+            </div>
+        </BrowserView>
+      </section>
+    </main>
+    </div >
   )
 }
